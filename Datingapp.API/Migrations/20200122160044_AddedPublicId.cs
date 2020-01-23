171798179f0b14e0ef8d01718799810386636a4d
Christopher Schnaars <cschnaars@labtesting.com>
2020-01-23 11:03:54 -0500

    End of Section 11.

---
+++ Datingapp.API/Migrations/20200122160044_AddedPublicId.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Datingapp.API.Migrations
+{
+    public partial class AddedPublicId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Lookingfor",
+                table: "Users",
+                newName: "LookingFor");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PublicID",
+                table: "Photos",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublicID",
+                table: "Photos");
+
+            migrationBuilder.RenameColumn(
+                name: "LookingFor",
+                table: "Users",
+                newName: "Lookingfor");
+        }
+    }
+}
